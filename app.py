3f36537118f46ad718ea41014748d5e80673497c
zlearningr <64264746+zlearningr@users.noreply.github.com>
2024-03-30 19:46:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ data = pd.read_csv('/Users/vincent/Desktop/BTC_traderecords.csv',index_col=0)
 for i in range(0,5):
     time.sleep(1)
     st.write('投资组合实时净值')
-    st.write(data.head(2)
+    st.write(data.head(2))
