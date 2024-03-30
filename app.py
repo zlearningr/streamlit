52ca98ac6fc8514ab863ad04d2eb590dd4f1683a
zlearningr <64264746+zlearningr@users.noreply.github.com>
2024-03-30 19:46:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import time
 
-for i in range(0,20):
+data = pd.read_csv('/Users/vincent/Desktop/BTC_traderecords.csv',index_col=0)
+
+for i in range(0,5):
     time.sleep(1)
     st.write('投资组合实时净值')
+    st.write(data.head(2)
